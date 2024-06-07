0db4a3b14f66e860b879c677691b87f3d2bb2b4c
Shubhangi Raj <shubhangi120989@gmail.com>
2024-06-07 21:34:19 +0530

    added cors url

--- firstBackend/src/app.js
+++ firstBackend/src/app.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser"
 const app=express()
 
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:`${process.env.CORS_ORIGIN}`,
     credentials:true
 }))
 // app.use(bodyParser.urlencoded({ extended: false }));
